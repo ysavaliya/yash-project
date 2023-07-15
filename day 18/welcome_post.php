8aac7e2a67eb71d5e1842720e5cdb74982fc61f9
ysavaliya <137284912+ysavaliya@users.noreply.github.com>
2023-07-15 13:34:57 +0530

    Delete day 18 directory

--- day 18/welcome_post.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-	Welcome <?php echo $_POST["fname"]," ", $_POST["lname"]; ?><br>
-	Your contact no. is: <?php echo $_POST["cntct"]; ?><br>
-	Your email address is: <?php echo $_POST["mail"]; ?>
-</body>
-</html>
