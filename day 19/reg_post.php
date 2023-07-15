7c9d171c4979ad7bdb8e4fb3caa9453a62d91345
ysavaliya <137284912+ysavaliya@users.noreply.github.com>
2023-07-15 13:37:30 +0530

    Add files via upload

---
+++ day 19/reg_post.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Registration Authencation</title>
+</head>
+<body>
+	<center>
+		<form action="welcome_post.php" method="post">
+			First name: <input type="text" name="fname" placeholder="Enter name"><br>
+			Last name: <input type="text" name="lname" placeholder="Enter name"><br>
+			Contact: <input type="text" name="cntct" placeholder="Enter Mobile no."><br>
+			Email id: <input type="text" name="mail" placeholder="Enter mail id"><br>
+			Password: <input type="password" name="psswd" placeholder="Enter pasword"><br>
+			<input type="submit">
+		</form>
+	</center>
+</body>
+</html>
---
+++ day 19/welcome_post.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+	Welcome <?php echo $_POST["fname"]," ", $_POST["lname"]; ?><br>
+	Your contact no. is: <?php echo $_POST["cntct"]; ?><br>
+	Your email address is: <?php echo $_POST["mail"]; ?>
+</body>
+</html>
